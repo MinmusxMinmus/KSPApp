664ea290b76393b23d2256bf1452158a6706f369
Min <minmusxminmus@hotmail.com>
2021-02-04 13:58:17 +0100

    Cleaned up code

--- src/other/util/Location.java
+++ src/other/util/Location.java
@@ -1,33 +1,77 @@
 package other.util;
 
+import java.util.StringJoiner;
+
+/**
+ * The Location class defines a rough point in the Kerbol system. The location of an object can be described by:<br>
+ * - The celestial body the object is orbiting, and<br>
+ * - If the object is in space, or not<br>
+ * With these two points of reference a location can be roughly determined, and consequently used for rendezvous
+ * checks and other relative position calculations.<br>
+ * <p>Notes: in the future, a Location object could include specific details such as orbit periapsis/apoapsis, orbit
+ *  * eccentricity, biome landed, water landing, and so on. This would in turn allow for more specific calculations, such
+ *  * as Delta-V requirements.</p>
+ */
 public class Location {
 
     private static final String DELIMITER = ":L:";
     private static final int ENCODE_FIELD_AMOUNT = 2;
 
+    /**
+     * Whether a vessel is in space or not, duhh.
+     */
     private final boolean inSpace;
+    /**
+     * The {@link CelestialBody} the vessel is currently at.
+     */
     private final CelestialBody celestialBody;
 
+    /**
+     * Generates an inmutable location object.
+     */
     public Location(boolean inSpace, CelestialBody celestialBody) {
         this.inSpace = inSpace;
         this.celestialBody = celestialBody;
     }
 
-    public static String toString(Location l) {
-        return l.inSpace + DELIMITER + l.celestialBody.name();
+    /**
+     * Generates a storable string from the given location.
+     */
+    public static String toString(Location location) {
+        StringJoiner joiner = new StringJoiner(DELIMITER);
+
+        joiner.add(Boolean.toString(location.inSpace));
+        joiner.add(location.celestialBody.name());
+
+
+        return joiner.toString();
     }
 
+    /**
+     * Generates a Location object from a storable string.
+     */
     public static Location fromString(String s) {
-        String[] split = s.split(DELIMITER);
-        if (split.length != ENCODE_FIELD_AMOUNT) return null;
-        return new Location(Boolean.parseBoolean(split[0]), CelestialBody.valueOf(split[1]));
+        String[] fields = s.split(DELIMITER);
+        if (fields.length != ENCODE_FIELD_AMOUNT) return null;
+        return new Location(
+                Boolean.parseBoolean(fields[0]),
+                CelestialBody.valueOf(fields[1])
+        );
     }
 
 
+    /**
+     * Returns whether the craft is currently landed at the given {@link CelestialBody}.
+     * @return {@code true} if the object is landed, {@code false} otherwise.
+     */
     public boolean landedAt(CelestialBody celestialBody) {
         return !inSpace && this.celestialBody.equals(celestialBody);
     }
 
+    /**
+     * Returns whether the craft is currently orniting the given {@link CelestialBody}.
+     * @return {@code true} if the object is in orbit, {@code false} otherwise.
+     */
     public boolean orbiting(CelestialBody celestialBody) {
         return !landedAt(celestialBody);
     }
@@ -38,6 +82,13 @@ public class Location {
 
     public CelestialBody getCelestialBody() {
         return celestialBody;
+    }
+
+    /**
+     * Returns whether the current craft is in contact with an atmosphere.
+     */
+    public boolean inAtmosphere() {
+        return !inSpace && celestialBody.hasAtmosphere();
     }
 
     @Override
