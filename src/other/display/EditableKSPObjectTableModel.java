feb290f0d850fe27f49daeb439dae3a05d25f9f7
Min <minmusxminmus@hotmail.com>
2021-02-18 12:21:56 +0100

    Updated to reflect Editable changes

--- src/other/display/EditableKSPObjectTableModel.java
+++ src/other/display/EditableKSPObjectTableModel.java
@@ -1,54 +1,61 @@
 package other.display;
 
-import other.display.KSPObjectTableModel;
-import other.interfaces.Editable;
+import other.KSPObject;
+import other.util.Field;
 
-public class EditableKSPObjectTableModel extends KSPObjectTableModel {
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
 
-    private final KSPObjectTableModel model = new KSPObjectTableModel();
-    private Editable object;
+public class EditableKSPObjectTableModel extends AbstractTableModel {
 
-    public EditableKSPObjectTableModel(Editable object) {
-        super();
+    private final KSPObject object;
+    private List<Field> fields;
+
+    public EditableKSPObjectTableModel(KSPObject object) {
         this.object = object;
-        model.setItem(object);
-    }
-
-    public Editable getItem() {
-        return object;
+        this.fields = object.getEditableFields();
     }
 
     @Override
     public int getRowCount() {
-        return model.getRowCount();
+        return fields.size();
     }
 
     @Override
     public int getColumnCount() {
-        return model.getColumnCount();
+        return 2;
     }
 
     @Override
     public String getColumnName(int column) {
-        return model.getColumnName(column);
+        return switch (column) {
+            case 0 -> "Name";
+            case 1 -> "Value";
+            default -> "???";
+        };
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        return model.getValueAt(rowIndex, columnIndex);
+        Field f = fields.get(rowIndex);
+        return switch (columnIndex) {
+            case 0 -> f.getName();
+            case 1 -> f.getValue();
+            default -> "???";
+        };
     }
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return columnIndex == 1 && !object.isEditable(rowIndex);
+        return columnIndex == 1;
     }
 
     @Override
     public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-        if (columnIndex != 2) return;
-        object.setField(rowIndex, aValue);
-        model.setItem(object);
+        if (columnIndex != 1) return;
+        Field editedField = fields.get(rowIndex);
+        object.setField(editedField.getName(), (String)aValue);
+        fields = object.getEditableFields();
         fireTableCellUpdated(rowIndex, columnIndex);
-        model.fireTableCellUpdated(rowIndex, columnIndex);
     }
 }
